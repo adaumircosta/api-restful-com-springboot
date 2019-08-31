cc0a81a95787ae278af745e629cd65d6a62f9137
Adaumir Costa <adaumircosta@gmail.com>
2019-08-31 11:39:26 -0400

    Criando o IndexController

---
+++ src/main/java/com/carros/api/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carros.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author adaumir
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    
+    @GetMapping
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+    
+}
